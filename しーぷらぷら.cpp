82b79507ddacc2955b8db627bcb5a7be08bd437c
mmnk <48mimineko@gmail.com>
2019-10-05 15:45:28 +0900

    bで割るようにした

--- しーぷらぷら.cpp
+++ しーぷらぷら.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 using ll = long long;
 int main(){
-    ll a;
-    cin >> a;
-    a /= 2;
+    ll a, b;
+    cin >> a >> b;
+    a /= b;
     cout << a << endl;
     return 0;
 }
